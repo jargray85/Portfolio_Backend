7b9fd808e2f991ceba6a6d3c6179e09b6d28b184
James Gray <jamesrjgray@gmail.com>
2023-06-24 12:15:01 -0500

    basic express server setup and working. Project data is retrieving

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+
+// Import JSON files
+const projects = require("./projects.json")
+
+// Create app object
+const app = express()
+
+// Middleware
+app.use(cors("*"))
+
+// Test/home route
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+// Route for retrieving project data
+app.get("/projects", (req, res) => {
+    res.json(projects)
+})
+
+// Port number
+const PORT = process.env.PORT || 4000
+
+// Turn on server
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
