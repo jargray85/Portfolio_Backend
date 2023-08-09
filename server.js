1389d3d0fe3ff89170acece8846b479deb48b8da
James Gray <jamesrjgray@gmail.com>
2023-08-08 21:21:46 -0400

    created new experience json data, route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 // Import JSON files
 const projects = require("./projects.json")
 const about = require("./about.json")
+const experience = require("./experience.json")
 
 // Create app object
 const app = express()
@@ -27,6 +28,11 @@ app.get("/projects", (req, res) => {
 // Route for retrieving about data
 app.get("/about", (req, res) => {
     res.json(about)
+})
+
+// Route for retrieving experience data
+app.get("/experience", (req, res) => {
+    res.json(experience)
 })
 
 // Port number
